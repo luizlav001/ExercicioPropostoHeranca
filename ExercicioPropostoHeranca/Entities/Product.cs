118806ea1ba2c9b9bfe9b952e01023dc78cfecbc
Lincoln doVale <lincolndovale07@gmail.com>
2021-01-18 18:05:33 -0300

    Adicionar arquivos de projeto.

---
+++ ExercicioPropostoHeranca/Entities/Product.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Globalization;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExercicioPropostoHeranca.Entities
+{
+    class Product
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+
+        public Product()
+        {
+
+        }
+
+        public Product(string name, double price)
+        {
+            Name = name;
+            Price = price;
+        }
+
+        public virtual string PriceTag()
+        {
+            return Name
+                + " $ "
+                + Price.ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
